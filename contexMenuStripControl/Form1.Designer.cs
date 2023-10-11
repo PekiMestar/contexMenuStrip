cc9f687d657b16ac4e766b0ece9ad411ea47471e
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-10-11 09:29:30 +0200

    Add files via upload

---
+++ contexMenuStripControl/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace contexMenuStripControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.motajLivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.motajDesnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox1.Location = new System.Drawing.Point(46, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox2.Location = new System.Drawing.Point(346, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.motajLivoToolStripMenuItem,
+            this.motajDesnoToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // motajLivoToolStripMenuItem
+            // 
+            this.motajLivoToolStripMenuItem.Name = "motajLivoToolStripMenuItem";
+            this.motajLivoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.motajLivoToolStripMenuItem.Text = "Motaj livo!!!";
+            this.motajLivoToolStripMenuItem.Click += new System.EventHandler(this.motajLivoToolStripMenuItem_Click);
+            // 
+            // motajDesnoToolStripMenuItem
+            // 
+            this.motajDesnoToolStripMenuItem.Name = "motajDesnoToolStripMenuItem";
+            this.motajDesnoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.motajDesnoToolStripMenuItem.Text = "Motaj desno!!!";
+            this.motajDesnoToolStripMenuItem.Click += new System.EventHandler(this.motajDesnoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 122);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem motajLivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem motajDesnoToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ contexMenuStripControl/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contexMenuStripControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void motajLivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = motajDesnoToolStripMenuItem.Text;
+        }
+
+        private void motajDesnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = motajDesnoToolStripMenuItem.Text;
+        }
+    }
+}
